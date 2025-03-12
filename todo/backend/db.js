a3364ec19677d4af95c8eed8a280b3a3965ce8cb
Yogender_ydv <yogender2603@gmail.com>
2025-03-12 22:05:26 +0530

    modified db

---
+++ todo/backend/db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+async function dbconnect() {
+    try {
+        await mongoose.connect('afdsgfnhdbgdsffd', {
+        });
+        console.log('Connected to DB');
+    } catch (err) {
+        console.error('Failed to connect:', err);
+    }
+}
+
+dbconnect();
+
+const todoschema = new mongoose.Schema({
+    title: { type: String, required: true },
+    done: { type: Boolean, default: false }
+});
+
+const userschema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    todo: [todoschema]
+});
+
+const User = mongoose.model('User', userschema);
+
+module.exports = { User };
